5233d422792f48b8592d3671dc7464550c20200b
JACKSON LONGO DOS SANTOS <jackson.longo.santos@gmail.com>
2021-04-14 19:41:33 -0300

    teste unitario

---
+++ TesteUnitarioTeste/FileProcessTeste.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TesteUnitarioDio;
+
+namespace TesteUnitarioTeste
+{
+    [TestClass]
+    public class FileProcessTeste
+    {
+
+        [TestMethod]
+        public void FileNameDoesExists()
+
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+            
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+            fromCall = fp.FileExists(@"C:\Regedit.exe");
+            Assert.IsFalse(fromCall);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowException()
+        {
+            FileProcess fp = new FileProcess();
+            fp.FileExists("");
+        }
+    }
+}
---
+++ TesteUnitarioTeste/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TesteUnitarioTeste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TesteUnitarioTeste")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("71197bea-d640-4144-b7b1-30ed63e9306f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
